dd3ab20c21f2b10e6799cb2f1566b29e20f1ec3a
AnishGane <anishgane10@gmail.com>
2025-07-04 21:59:12 +0545

    Update CORS configuration in server.js to include the production frontend URL for enhanced accessibility.

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,11 @@ connectCloudinary();
 
 // CORS configuration
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"], // Add your frontend URLs
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://softstitch-ecommerce-frontend.onrender.com",
+  ], // Add your frontend URLs
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "token"],
   credentials: true,

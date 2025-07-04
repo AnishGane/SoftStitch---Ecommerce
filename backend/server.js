609eedb784cb5de8badad34979e58253e89f96ec
AnishGane <anishgane10@gmail.com>
2025-07-04 22:33:18 +0545

    Update CORS configuration in server.js to correct the production frontend URL for improved accessibility.

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
-    "https://softstitch-ecommerce-frontend.onrender.com",
+    "https://softstitch-ecommerce.onrender.com",
   ], // Add your frontend URLs
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "token"],

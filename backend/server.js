bd927ffd1f11e350fd1f3e6c5af756dbacbb45b9
AnishGane <anishgane10@gmail.com>
2025-07-04 23:24:44 +0545

    Update CORS configuration in server.js to include additional admin frontend URL for improved accessibility.

--- backend/server.js
+++ backend/server.js
@@ -21,6 +21,7 @@ const corsOptions = {
     "http://localhost:5173",
     "http://localhost:5174",
     "https://softstitch-ecommerce.onrender.com",
+    "https://softstitch-ecommerce-admin.onrender.com",
   ], // Add your frontend URLs
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "token"],

15bbd595a26f3ee6ee191a7d50cab58f28ecb17c
Anish Gane <anishgane10@gmail.com>
2026-01-04 18:47:04 +0545

    Update backend CORS configuration in server.js to include new admin frontend URL and modify .env file to point to the production backend URL.

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,10 @@ connectCloudinary();
 const corsOptions = {
   origin: [
     "http://localhost:5173",
-    // "http://localhost:5174",
+    "http://localhost:5174",
     // "http://192.168.0.116:5173",
     "https://soft-stitch-ecommerce.vercel.app",
+    "https://soft-stitch-ecommerce-admin.vercel.app",
   ], // Add your frontend URLs
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "token"],
